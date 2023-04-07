067743d85795debb9155e028ce55338c986d51cf
marian <marian.slavich@gmail.com>
2023-04-07 12:08:34 +0300

    Добавьте файлы проекта.

---
+++ Lab4OOPcSharp/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Lab4OOPcSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int numberOfColumnsWithZeroElements = 0;
+            int rowNumberWithMaxEqualElements = 0;
+            int maxEqualElements = 0;
+            int currentEqualElements = 0;
+            int n = 5;
+            int m = 5;
+            Random random = new Random();
+            int[,] matrix = new int[n, m];
+            FillMatrix(n, m, random, matrix);
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == 0)
+                    {
+                        numberOfColumnsWithZeroElements++;
+                    }
+
+                    if (j > 0 && matrix[i, j] == matrix[i, j - 1])
+                    {
+                        currentEqualElements++;
+                    }
+                    else
+                    {
+                        if (currentEqualElements > maxEqualElements)
+                        {
+                            maxEqualElements = currentEqualElements;
+                            rowNumberWithMaxEqualElements = i;
+                        }
+                        currentEqualElements = 1;
+                    }
+
+                }
+            
+
+
+            }
+            dataGridView1.RowCount = matrix.GetLength(0);
+            dataGridView1.ColumnCount = matrix.GetLength(1);
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    dataGridView1[j, i].Value = matrix[i, j];
+                }
+            }
+
+            textBox1.Text = numberOfColumnsWithZeroElements.ToString();
+            textBox2.Text = rowNumberWithMaxEqualElements.ToString();
+            textBox3.Text = FindMainDiagElem(matrix, n).ToString();
+        }
+
+        private static void FillMatrix(int n, int m, Random random, int[,] matrix)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i, j] = random.Next(0, 9);
+                }
+            }
+        }
+
+        public double FindMainDiagElem(int[,] matrix,int n)
+        {
+            double sum=0;
+            for(int i = 0; i < n; i++)
+            {
+                sum+=matrix[i,i];
+            }
+            return sum / n;
+        }
+    }
+}
---
+++ Lab4OOPcSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Lab4OOPcSharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab4OOPcSharp")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("8919d2a2-fb28-4d32-9c6e-cc83b5cc7261")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
